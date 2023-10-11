7b93daa8be8467216fd7fb8c872b143e05d648f3
tarikul-islam <tariikul12@gmail.com>
2023-10-11 15:14:02 +0600

    landing page working

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:["images.unsplash.com"]
+        domains:["images.unsplash.com", "s3.us-west-2.amazonaws.com"]
     }
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import GameCard from "@/components/GameCard/GameCard";
+import GameCategoryCard from "@/components/GameCategory/GameCategoryCard";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,7 +7,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-<HeroSection/>
+<HeroSection ShowLink/>
 <section className={sectionClassName.section}>
   <div className={sectionClassName.trending}>
 <h2 className={sectionClassName.trendingTitle}>
@@ -42,10 +43,50 @@ price={game.price}/>)}
     </Link>
   </div>
 </section>
-<section  style={{
+<section className={styles.categorySection}  style={{
   backgroundImage:"url('https://images.unsplash.com/photo-1556438064-2d7646166914?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80')"
 }}>
+<div className={styles.categoryContent}>
+  <h2 className={styles.categoryHeading}>categories
+  </h2>
+  <p className={styles.categorySubHeading}>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores magnam, nemo officia tempore aperiam quod reprehenderit consequatur fugiat cumque ea!
+  </p>
+  <div className="flex flex-wrap">
+    {
+      categories.map((category)=>(
+        <GameCategoryCard
+        key={category.id}
+        categoryImage={category.image}
+        slug={category.slug}
+        categoryName={category.categoryName}
+        />
+      )
+      )
+    }
+  </div>
+   </div>
+</section>
+<section id='recent=games' className={recentGamesClasses.section}>
+<h2 className={recentGamesClasses.section}>Our Recent Games</h2>
+<p> Stay Ahead Caming Curve with Our Latest Cames</p>
+<div className="flex rounded gap-8 flex-wrap py-10">
+  {
+    games.map(game =>(
+      <GameCard
+      key={game.id}
+      gameName={game.name}
+      imageUrl={game.image}
+      slug={game.slug}
+      price={game.price}
+      />
+    ))
+  }
 
+</div>
+  <Link href='games' className={sectionClassName.latestButton} >
+    See All
+  </Link>
 </section>
     </main>
   )
@@ -107,3 +148,39 @@ const featuredClassName ={
     featuredContent:"",
     featuredDescription:""
 }
+
+const styles ={
+  categorySection:'',
+  categoryContent: '',
+  categoryHeading:'',
+  categorySubHeading:''
+}
+
+const recentGamesClasses ={
+  section:"",
+  heading:"",
+  subHeading:""
+}
+
+const categories =[
+  {
+    id:1,
+    categoryName:"Action",
+    slug:'action',
+  image:'https://images.unsplash.com/photo-1542751371-adc38448a05e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Z2FtaW5nfGVufDB8fDB8fHww&w=1000&q=80'
+  },
+  {
+    id:2,
+    categoryName:"Adventure",
+    slug:'adventure',
+  image:'https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1560253023-3ec5d502959f'
+  },
+  {
+    id:3,
+    categoryName:"Tray",
+    slug:'tray',
+  image:'https://images.unsplash.com/photo-1636487658547-c05ee4cdc1ac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGVzcG9ydHN8ZW58MHx8MHx8fDA%3D&w=1000&q=80'
+  },
+
+]
+
